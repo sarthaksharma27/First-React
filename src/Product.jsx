4029fea9ca91bba8ec963a321f2f07e4b2c9a6df
sarthak sharma <sharma.sarthak222872@gmail.com>
2023-11-02 10:57:40 +0530

    right way to add li

--- src/Product.jsx
+++ src/Product.jsx
@@ -1,14 +1,13 @@
-import "./Product.css";
+import './Product.css';
 
-function Product({title, price, features, features2}) {
-    return (
-        <div className="Product">
-            <h3>{title}</h3>
-            <p>Price: {price}</p>
-            <p>{features}</p>
-            {/* <p>{features2.b}</p> */}
-        </div>
-    );
+function Product({ title, price, features }) {
+  return (
+    <div className="Product">
+      <h3>{title}</h3>
+      <p>Price: {price}</p>
+      {/* <p>{features.map((feature) => <li>{feature}</li>)}</p> */}
+    </div>
+  );
 }
 
 export default Product;
--- src/ProductTab.jsx
+++ src/ProductTab.jsx
@@ -2,16 +2,16 @@ import React from 'react';
 import Product from './Product.jsx';
 
 function ProductTab() {
-  let options = [<li>hi-tech</li>, <li>durable</li>, <li>fast</li> ];
-  // let options2 = { a: 'hi-tech', b: 'durable', c: 'fast' };
+  let options = ['hi-tech', 'durable', 'fast'];
 
   return (
     <>
-      <Product title="phone" price={30000} features={options}  />
-      <Product title="laptop" price={40000} features={options}  />
-      <Product title="pen" price={10} />
+      <Product title="phone" price={30000} features={options} />
+      <Product title="laptop" price={40000} features={options} />
+      <Product title="pen" price={10} features={[]} />
     </>
   );
 }
 
 export default ProductTab;
+
