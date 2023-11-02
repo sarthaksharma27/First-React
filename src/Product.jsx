364af45ef3036dfa2d06db495175d170490375f3
sarthak sharma <sharma.sarthak222872@gmail.com>
2023-11-02 10:35:48 +0530

    passing an array to props with li init

--- src/Product.jsx
+++ src/Product.jsx
@@ -1,10 +1,12 @@
 import "./Product.css";
 
-function Product({title, price}) {
+function Product({title, price, features, features2}) {
     return (
         <div className="Product">
             <h3>{title}</h3>
             <p>Price: {price}</p>
+            <p>{features}</p>
+            {/* <p>{features2.b}</p> */}
         </div>
     );
 }
--- src/ProductTab.jsx
+++ src/ProductTab.jsx
@@ -1,13 +1,17 @@
-import Product from "./Product.jsx";
+import React from 'react';
+import Product from './Product.jsx';
 
 function ProductTab() {
-    return (
-      <>
-        <Product title="phone" price="30,000"/>
-        <Product title="laptop" price="40,000"/>
-        <Product title="pen" price="10"/>
-      </>
-    )
+  let options = [<li>hi-tech</li>, <li>durable</li>, <li>fast</li> ];
+  // let options2 = { a: 'hi-tech', b: 'durable', c: 'fast' };
+
+  return (
+    <>
+      <Product title="phone" price={30000} features={options}  />
+      <Product title="laptop" price={40000} features={options}  />
+      <Product title="pen" price={10} />
+    </>
+  );
 }
 
 export default ProductTab;
