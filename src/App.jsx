48351e7e923eebb186e6e3fced90570e998fb6d9
sarthak sharma <sharma.sarthak222872@gmail.com>
2023-11-02 12:35:09 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import "./App.css"
 import ProductTab from "./ProductTab.jsx";
+import Hello from "./Hello.jsx";
+import AmazonTab from "./AmazonTab.jsx";
 
 function App() {
   return (
     <>
-      <ProductTab/>
+      {/* <ProductTab/>
+      <Hello userName="Sarthak" textColor="yellow"/> */}
+      <AmazonTab />
+
     </>
   )
 }
---
+++ src/Hello.jsx
@@ -0,0 +1,8 @@
+function Hello({ userName, textColor }) {
+    return (
+      <p style={{ color: textColor }}>{userName}</p>
+    );
+  }
+  
+  export default Hello;
+  
