bb17f7b1618acc04c7ba2f120eefc26997d864a4
sarthak sharma <sharma.sarthak222872@gmail.com>
2023-11-04 11:01:37 +0530

    click event

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import "./App.css"
 import ProductTab from "./ProductTab.jsx";
+import Button from "./Button.jsx";
 
 function App() {
   return (
     <>
       <h2>Blockbuster Deals | Shop Now</h2>
       <ProductTab/>
+      <Button />
     </>
   )
 }
